a6e6edc4c212d34cc7b19749da7756a32c5bc85f
devinizazradindewantoro <darkdevin469@gmail.com>
2023-09-11 09:18:38 +0700

    Math

--- MyFirstJavaGithub10.java
+++ MyFirstJavaGithub10.java
@@ -1,6 +1,10 @@
 public class MyFirstJavaGithub10 {
     public static void main (String [] args) {
         System.out.println("Hello From Github") ;
+
+        int x = 100;
+        int y = 87;
+        System.out.print(x * y);
     }
 
 }
